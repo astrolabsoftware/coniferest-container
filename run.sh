adf6b4efeb98fb9c5499712dd058ab5c49707057
Your Name <you@example.com>
2025-07-25 16:36:14 +0200

    Update current feature

--- run.sh
+++ run.sh
@@ -13,12 +13,23 @@ ciux ignite "$PROJECT_DIR"
 
 . $PROJECT_DIR/.ciux.d/ciuxconfig.sh
 
-echo "Using local image $IMAGE"
-echo "oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO"
-echo "   Welcome in k8s toolbox desk"
-echo "oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO"
+MOUNTS="$MOUNTS --volume /data:$DIR/data"
+MOUNTS="$MOUNTS --volume /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro"
+
+echo "Using image $CIUX_IMAGE_URL"
 docker run -it \
+    --name coniferest
     $MOUNTS --rm \
     --user=$(id -u):$(id -g $USER) \
     -w $HOME -- \
-    "$IMAGE"
+    "$CIUX_IMAGE_URL"
+
+ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' coniferest)
+
+echo "oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO"
+echo "  Welcome in coniferest"
+echo "  Connect through your web browser to: http://$ip:8888"
+echo "oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO"
+
+
+
